50e2997f63171d182eba4122f8952fb17d733d5a
albina77 <57559844+albina77@users.noreply.github.com>
2020-11-24 00:17:17 +0300

    fixed w9_3 it two variants, added comments for w9_2

--- w9/w9_2.py
+++ w9/w9_2.py
@@ -1,8 +1,10 @@
 import zipfile as zf
 import os
 import string
-class TextLoader:
+from pathlib import Path
 
+
+class TextLoader:
     way = None
     my_zip_file = None
 
@@ -12,21 +14,32 @@ class TextLoader:
         self.my_zip_file.extractall()
 
     def __len__(self):
-        t =  os.listdir(self.way.rstrip('.zip'))
+        t = os.listdir(self.way.rstrip('.zip'))
         return len(t)
 
     def __next__(self):
         tmp = os.listdir(self.way.rstrip('.zip'))
         for i in tmp:
-            with open(self.way.rstrip('.zip') + '\\' + i, 'rt', encoding = 'utf8') as j:
+            with open(self.way.rstrip('.zip') + '\\' + i, 'rt', encoding='utf8') as j:
+                # удаляем из way расширение ".zip" -> получаем название соответствующей папки
+                # i - название файла в нынешней итерации
                 tt = str.maketrans(dict.fromkeys(string.punctuation))
-                return j.read().lower().translate(tt)
+                # string.punctuation - даёт строку из всех занков препинания
+
+                # dict.fromkeys(string.punctuation) - создаёт словарь, где ключами являются символы
+                # строки string.punctuation
+
+                # str.maketrans - задаёт "таблицу", обозначающую, какой символ (ключ) на что необходимо заменить (зн-е)
+                return j.read().lower().translate(tt)  # .lower() - приводит строки к нижнему регистру,
+                # .translate() - заменяет в тексте знаки пунктуации на None в соответствии с таблицей tt
         raise StopIteration()
 
     def __iter__(self):
         return self
 
 
-a = TextLoader('C:\\Users\\79821\\FilesForGit\\MIPT_Python\\w9\\sample.zip')
+basedir = str(Path(__file__).parent)
+basedir = os.path.join(basedir, 'sample.zip')
+a = TextLoader(basedir)
 print(len(a))
 print(next(a))
